99ef720ea2b8643f77af319cbaadcc4a5b71e64d
yogeshsonnis <silverlightyogesh@gmail.com>
2017-04-17 17:23:00 +0530

    DashboardATM project added

---
+++ Design_dashboardATM_AIFile/Design_dashboardATM_AIFile/CommonControl/CustomWindow.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Design_dashboardATM_AIFile.CommonControl
+{
+    public class CustomWindow : Window
+    {
+        static CustomWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomWindow), new FrameworkPropertyMetadata(typeof(CustomWindow)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var btnClose = this.GetTemplateChild("PART_CLOSE") as Button;
+            var btnMinimize = this.GetTemplateChild("PART_MINIMIZE") as Button;
+            var btnMaximize = this.GetTemplateChild("PART_MAXIMIZE_RESTORE") as Button;
+            //var btnDragMove = this.GetTemplateChild("PART_TITLEBAR") as Button;
+
+            btnMinimize.Click += btnMinimize_Click;
+
+            btnClose.Click += btnClose_Click;
+            btnMaximize.Click += btnmaximize_Click;
+            //btnDragMove.Click += btnDragMove_Click;
+
+        }
+
+        void btnDragMove_Click(object sender, RoutedEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        void btnmaximize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = (WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
---
+++ Design_dashboardATM_AIFile/Design_dashboardATM_AIFile/Design_dashboardATM.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Design_dashboardATM_AIFile.CommonControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Design_dashboardATM_AIFile
+{
+    /// <summary>
+    /// Interaction logic for Design_dashboardATM.xaml
+    /// </summary>
+    public partial class Design_dashboardATM : CustomWindow
+    {
+        public Design_dashboardATM()
+        {
+            InitializeComponent();
+        }
+    }
+}
